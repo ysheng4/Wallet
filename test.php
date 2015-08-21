295bab20899d14125e4114a8331affce1e8fb3a5
Yingjie Sheng <aegeanjie@gmail.com>
2015-08-21 15:37:19 -0500

    test
    www

---
+++ test.php
@@ -0,0 +1,17 @@
+$peixunxiangmu = rtrim($peixunxiangmu, ','); 
+
+$peixunxiangmuarr = explode(',',$peixunxiangmu);
+$jianianq='';
+if( $peixunxiangmu !="0" and $peixunxiangmu !=""){
+foreach($peixunxiangmuarr as $k=>$v){
+	if(!empty($v)){
+		if($jianianq==''){
+			$jianianq .=" `pxxm`='$v' ";
+		}else{
+		$jianianq .=" or 'pxxm`='$v' ";  
+		}
+	}
+	
+}
+	$wq .=" and (".$jianianq.") ";    
+}
